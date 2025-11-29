501d3e2f7254b7cf582f39b3e686ef0013a9928f
marv <marv.kopka@web.de>
2025-11-29 19:07:27 +0100

    add test

---
+++ MessagingToolkit.Tests/Messengers/MessengerTest.cs
@@ -0,0 +1,136 @@
+﻿using MessagingToolkit.Messengers;
+
+namespace MessagingToolkit.Tests.Messengers;
+
+[TestFixture]
+[TestOf(typeof(IMessenger))]
+public abstract class MessengerTest<T>
+    where T : IMessenger, new()
+{
+    protected T Fixture { get; private set; }
+
+    [SetUp]
+    public void SetUp()
+    {
+        Fixture = new T();
+    }
+
+    [Test]
+    public void ChainedPublish()
+    {
+        var counter = 0;
+
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Register<OtherTestMessage>(this, _ => Fixture.Publish(new TestMessage()));
+
+        Fixture.Publish(new OtherTestMessage());
+
+        Assert.That(counter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task ChainedAsyncPublish()
+    {
+        var holder = new Holder();
+
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+        Fixture.Register<OtherTestMessage>(this, async _ => await Fixture.PublishAsync(new TestMessage()));
+
+        await Fixture.PublishAsync(new OtherTestMessage());
+
+        Assert.That(holder.Counter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestUnregister()
+    {
+        var counter = 0;
+
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Unregister<TestMessage>(this);
+
+        Fixture.Publish(new TestMessage());
+
+        Assert.That(counter, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void TestPublish()
+    {
+        var counter = 0;
+
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+
+        Fixture.Publish(new TestMessage());
+
+        Assert.That(counter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task TestPublishAsync()
+    {
+        var holder = new Holder();
+
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+
+        await Fixture.PublishAsync(new TestMessage());
+
+        Assert.That(holder.Counter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestPublishMultiple()
+    {
+        var counter = 0;
+        var holder = new Holder();
+
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+
+        Fixture.Publish(new TestMessage());
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(counter, Is.EqualTo(2));
+            Assert.That(holder.Counter, Is.EqualTo(2));
+        }
+    }
+
+    [Test]
+    public async Task TestPublishAsyncMultiple()
+    {
+        var counter = 0;
+        var holder = new Holder();
+
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+        Fixture.Register<TestMessage>(this, _ => Increase(holder));
+
+        await Fixture.PublishAsync(new TestMessage());
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(counter, Is.EqualTo(2));
+            Assert.That(holder.Counter, Is.EqualTo(2));
+        }
+    }
+
+    private async Task Increase(Holder counter)
+    {
+        counter.Counter += 1;
+
+        await Task.Delay(1);
+    }
+
+    private class Holder
+    {
+        public int Counter { get; set; }
+    }
+
+    private record TestMessage;
+
+    private record OtherTestMessage;
+}
--- MessagingToolkit.Tests/Messengers/WeakReferenceMessengerTest.cs
+++ MessagingToolkit.Tests/Messengers/WeakReferenceMessengerTest.cs
@@ -4,136 +4,29 @@ namespace MessagingToolkit.Tests.Messengers;
 
 [TestFixture]
 [TestOf(typeof(WeakReferenceMessenger))]
-public class WeakReferenceMessengerTest
+public class WeakReferenceMessengerTest : MessengerTest<WeakReferenceMessenger>
 {
-    private WeakReferenceMessenger _sut;
-
-    [SetUp]
-    public void SetUp()
-    {
-        _sut = new WeakReferenceMessenger();
-    }
-
     [Test]
-    public void TestUnregister()
+    public void TestPublishIgnoresRemovedReferences()
     {
         var counter = 0;
 
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Unregister<TestMessage>(this);
+        Register(_ => Interlocked.Increment(ref counter));
+        Fixture.Register<TestMessage>(this, _ => Interlocked.Increment(ref counter));
 
-        _sut.Publish(new TestMessage());
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
 
-        Assert.That(counter, Is.EqualTo(0));
-    }
-
-    [Test]
-    public void ChainedPublish()
-    {
-        var counter = 0;
-
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Register<OtherTestMessage>(this, _ => _sut.Publish(new TestMessage()));
-
-        _sut.Publish(new OtherTestMessage());
+        Fixture.Publish(new TestMessage());
 
         Assert.That(counter, Is.EqualTo(1));
     }
 
-    [Test]
-    public async Task ChainedAsyncPublish()
+    private void Register(Action<TestMessage> action)
     {
-        var holder = new Holder();
-
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-        _sut.Register<OtherTestMessage>(this, async _ => await _sut.PublishAsync(new TestMessage()));
-
-        await _sut.PublishAsync(new OtherTestMessage());
-
-        Assert.That(holder.Counter, Is.EqualTo(1));
-    }
-
-    [Test]
-    public void TestPublish()
-    {
-        var counter = 0;
-
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Publish(new TestMessage());
-
-        Assert.That(counter, Is.EqualTo(1));
-    }
-
-    [Test]
-    public async Task TestPublishAsync()
-    {
-        var holder = new Holder();
-
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-
-        await _sut.PublishAsync(new TestMessage());
-
-        Assert.That(holder.Counter, Is.EqualTo(1));
-    }
-
-    [Test]
-    public void TestPublishMultiple()
-    {
-        var counter = 0;
-        var holder = new Holder();
-
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-
-        _sut.Publish(new TestMessage());
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(counter, Is.EqualTo(2));
-            Assert.That(holder.Counter, Is.EqualTo(2));
-        }
-    }
-
-    [Test]
-    public async Task TestPublishAsyncMultiple()
-    {
-        var counter = 0;
-        var holder = new Holder();
-
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Register<TestMessage>(this, _ => Increase(ref counter));
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-        _sut.Register<TestMessage>(this, _ => Increase(holder));
-
-        await _sut.PublishAsync(new TestMessage());
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(counter, Is.EqualTo(2));
-            Assert.That(holder.Counter, Is.EqualTo(2));
-        }
-    }
-
-    private void Increase(ref int counter)
-    {
-        Interlocked.Increment(ref counter);
-    }
-
-    private async Task Increase(Holder counter)
-    {
-        counter.Counter += 1;
-
-        await Task.Delay(1);
-    }
-
-    private class Holder
-    {
-        public int Counter { get; set; }
+        var recipient = new object();
+        Fixture.Register(recipient, action);
     }
 
     private record TestMessage;
-
-    private record OtherTestMessage;
 }
